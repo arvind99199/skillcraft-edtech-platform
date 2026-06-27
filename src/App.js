c28dac3ef321c89ed1a894e0f89268fc1968eb7e
arvind99199 <arvindpandey99199@gmail.com>
2026-06-28 01:15:27 +0530

    favicon added

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ function App() {
 
 
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  //const dispatch = useDispatch();
+  //const navigate = useNavigate();
   
   const { user } = useSelector((state) => state.profile)
 
