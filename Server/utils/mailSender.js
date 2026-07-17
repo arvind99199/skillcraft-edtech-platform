b7a3734a8efcf1ec3370c8d89d535bc95c25ded8
arvind99199 <arvindpandey99199@gmail.com>
2026-07-17 23:14:24 +0530

    changes in mailsender

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -8,6 +8,8 @@ const mailSender = async (email, title, body) => {
         let transporter = nodemailer.createTransport({
 
             host : process.env.MAIL_HOST,
+            port: 465,
+            secure: true,
             auth : {
                 user : process.env.MAIL_USER,
                 pass : process.env.MAIL_PASS,
